0fcb997b1e609e18cb378899a0dbc4a48d531a35
jean-rodriguez kinouani <kinouani@gmail.com>
2022-08-16 12:59:04 +0200

    add action index show create new

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "index", to: "tasks#index"
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  get "tasks/:id", to: "tasks#show", as: :task
+
 end
+
